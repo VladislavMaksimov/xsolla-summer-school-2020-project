44875be85ba3910853f6386643df3047d175e0cc
Vladislav Maksimov <workmaximov@yandex.ru>
2020-08-14 16:26:30 +0500

    Trying to get data.

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,14 +1,28 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 import { cards } from '../scss/style.scss'
 
 import { Card } from './Card'
 
+const path = "https://raw.githubusercontent.com/xsolla/xsolla-frontend-school-2020/master/events.json";
+
 // Контейнер с карточками
 export const CardsContainer = () => {
+    const [cardsCollection, getCards] = useState({});
+
+    useEffect(() => {
+        fetch(path)
+          .then(response => {getCards(response.json())});
+    }, [path])
+
     return(
-        <div className="cards">
-            <Card/>
+        <div className={cards}>
+        {
+            Object.keys(cardsCollection).map((key) =>{
+                const card = cardsCollection[key];
+                <Card info={card} source={card.image}/>
+            })
+        }
         </div>
     )
 }
