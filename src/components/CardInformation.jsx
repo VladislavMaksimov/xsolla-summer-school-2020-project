d976f86fe107d45bd120a6b083107e0c19297640
Vladislav Maksimov <workmaximov@yandex.ru>
2020-08-22 00:48:18 +0500

    JSON filtered.

--- src/components/CardInformation.jsx
+++ src/components/CardInformation.jsx
@@ -2,29 +2,35 @@ import React from 'react'
 import { NavLink } from "react-router-dom"
 import { InfoContext } from "../context/InfoContext"
 import { info_page, info_card, info_card_img, info_card_text } from "../scss/style.scss"
+import { useParams } from 'react-router-dom'
 
 export const CardInformation = () => {
+    const {id} = useParams();
+
     return (
         <InfoContext.Consumer>
             {(cardsInfo) =>
             {
+                // фильтруем json по id
+                const cardsJson = { array: cardsInfo }
+                const cardInfo = Object.values(cardsJson.array).filter((card) => card.id === id)
                 
                 (
                 <div className = { info_page }>
                     <div className = { info_card }>
 
                         <div className = { info_card_img }>
-                            <img src = { cardsInfo.image }/>
+                            <img src = { cardInfo.image }/>
                         </div>
 
-                        <h3> { cardsInfo.name } </h3>
+                        <h3> { cardInfo.name } </h3>
 
                         <br/>
 
                         <span className = { info_card_text }>
-                            Date: { cardsInfo.date } <br/> <br/>
-                            City: { cardsInfo.city } <br/> <br/>
-                            Genre: { cardsInfo.genre }
+                            Date: { cardInfo.date } <br/> <br/>
+                            City: { cardInfo.city } <br/> <br/>
+                            Genre: { cardInfo.genre }
                         </span>
 
                         <NavLink exact to="/">
