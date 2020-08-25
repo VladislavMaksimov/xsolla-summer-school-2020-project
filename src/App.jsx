627e7c1316d52ca513cb5894a2e65e27b7a2148e
Galieva Yulia <y.galieva@xsolla.com>
2020-08-25 19:42:26 +0500

    task with props using only

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { hot } from 'react-hot-loader/root'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Switch, Route, HashRouter, BrowserRouter } from 'react-router-dom'
 
 import { Page } from './components/Page'
 import { InfoRoutes } from './components/InfoRoutes'
+const path = 'https://raw.githubusercontent.com/xsolla/xsolla-frontend-school-2020/master/events.json'
 
 const App = () => {
+    const [cardsInfo, setInfo] = useState({})
+
+    // Получаем данные из json-файла
+    useEffect(() => {
+        fetch(path)
+            .then(response => response.json())
+            .then(response => setInfo(response))
+    }, [path])
+
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
           <Page/>
         </Route>
-        <InfoRoutes/>
+        <InfoRoutes cardsInfo={cardsInfo}/>
       </Switch>
     </BrowserRouter>
   )
--- src/components/InfoRoutes.jsx
+++ src/components/InfoRoutes.jsx
@@ -1,19 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { CardInformation } from './CardInformation'
 import { Route } from 'react-router-dom'
 
-const path = 'https://raw.githubusercontent.com/xsolla/xsolla-frontend-school-2020/master/events.json'
-
-export const InfoRoutes = () => {
-  const [cardsInfo, setInfo] = useState({})
-
-  // Получаем данные из json-файла
-  useEffect(() => {
-    fetch(path)
-      .then(response => response.json())
-      .then(response => setInfo(response))
-  }, [path])
-
+export const InfoRoutes = ({cardsInfo}) => {
   return (Object.values(cardsInfo).map((card) => {
     return (
       <Route exact path={`/card_info/${card.id}`} key={card.id}>
